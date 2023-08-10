15eb80cf9de5122ed0a3c4ada9f74ea85991d3f5
ymarquina <yeseniamrqna@gmail.com>
2023-08-10 14:29:29 -0700

    fix game play and display

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-
+import '../App.css'
 
 class ScoreBoard extends Component{
 
     render(){
 
-        let { score } = this.props;
+        let { score, highScore } = this.props;
 
         return(
             <div className="score-board">
-                <p>{score}</p>
+                <div>
+                    <h1>MemeMinder: The LOL Memory Game</h1>
+                </div>
+                <div className="score">
+                <p>Current Score:{score}</p>
+                <p>High Score: {highScore}</p>
+                </div>
             </div>
         )
     }
